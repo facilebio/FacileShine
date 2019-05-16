d1e1d55ec33485f0581adb87c6ab33aaaf0bfaaf
lianos <lianoglou@dnli.com>
2019-05-15 23:57:22 -0700

    Fixed state double-dipping in assaySelect.
    There is an antipattern that needs to be avoided, which is to have
    have the same reactive block depend on both an `input$something`
    and the internal `state$something` equivalent.
    
    This block:
    
    ```
    assay_info <- eventReactive(list(input$assay, state$assay_info), {
      # ...
    })
    ```
    
    Was bad because both the input$ and state$ were colliding with each other.
    It was split into two blocks:
    
    ```
    observeEvent(input$assay, {
      # which does some junk here and updates
      # state$assay_info, if necessary
    })
    
    assay_info <- reactive(state$info)
    ```
    
    That's the gist of it

--- R/assaySelect-module.R
+++ R/assaySelect-module.R
@@ -24,52 +24,80 @@ assaySelect <- function(input, output, session, rfds, ..., .reactive = TRUE)  {
       feature_id = character(),
       name = character()))
 
-  # This a tibble of the available assays available for the active_samples
-  # of the rfds
-  assays <- reactive({
+  # The names of the assays available over the current samples
+  assay.names <- reactive({
     req(initialized(rfds))
     aa <- req(isolate.(active_assays(rfds)))
-    ftrace("Updating available set of assays stored internally: ",
-           paste(aa$assay, collapse = ","))
-    aa
+    anames <- aa$assay
+    ftrace("Updating set of available assay (names): ",
+           paste(anames, collapse = ","))
+    anames
+  })
+
+  # The currently active cohort of smaples may have no assay data!
+  no.assays <- reactive({
+    req(initialized(rfds))
+    length(assay.names()) == 0L
   })
 
   # Update the assay choice dropdown if necessary when the underlying samples
   # change. If the currently selected assay is still available, then keep it
   # selected.
-  observe({
-    available_assays <- assays()$assay
-    .ai <- isolate(state$assay_info)
-    if (.ai$assay %in% available_assays) {
+  observeEvent(assay.names(), {
+    available_assays <- assay.names()
+    .ai <- state$assay_info
+
+    if (no.assays()) {
+      fwarn("{bold}{red}No assays available for current active_samples{reset}")
+      selected <- NULL
+      update.state <- nrow(.ai) != 0L
+    } else if (.ai$assay %in% available_assays) {
       selected <- .ai$assay
+      update.state <- .ai$assay != selected
     } else {
       selected <- available_assays[1L]
+      update.state <- TRUE
     }
 
-    if (isolate(state$assay_info$assay) != selected) {
+    if (update.state) {
       ftrace("A change in available assays (assays()$assay) changes ",
              "{red}{bold}assay_info state{reset} variable to: {bold}",
              selected, "{reset}")
-      state$assay_info <- FacileData::assay_info(rfds, selected)
+      .ai <- collect(FacileData::assay_info(rfds, selected), n = Inf)
+      if (is.null(selected)) {
+        .ai <- filter(.ai, FALSE)
+      }
+      state$assay_info <- .ai
     }
 
     updateSelectInput(session, "assay", choices = available_assays,
                       selected = selected)
+  }, priority = 10)
+
+  # beware that the available assays may be the empty set
+  observeEvent(input$assay, {
+    req(initialized(rfds))
+    selected <- input$assay
+    if (unselected(selected) && !no.assays()) {
+      # We aren't completely initialized yet, bail
+      req(FALSE)
+    }
+
+    .ai <- state$assay_info
+    if (!isTRUE(selected == .ai$assay)) {
+      ftrace("New assay selected from input changes {red}{bold}state{reset}",
+             "{bold}", .ai$assay, "->", selectred, "{reset}")
+      if (unselected(selected)) selected <- NULL
+      .ai <- collect(FacileData::assay_info(rfds, selected), n = Inf)
+      if (is.null(selected)) .ai <- filter(.ai, FALSE)
+      state$assay_info <- .ai
+    }
   })
 
-  assay_info <- eventReactive(list(input$assay, state$assay_info), {
-    req(!unselected(input$assay_info$assay))
-    .assay <- req(input$assay)
-    .ai <- state$assay_info
-    if (.ai$assay != .assay) {
-      ftrace("upated selected assay from input changes {red}{bold}state{reset}")
-      state$assay_info <- FacileData::assay_info(rfds, .assay)
-    }
-    state$assay_info
-  })
+  assay_info <- reactive(state$assay_info)
 
   features <- reactive({
-    assay_info. <- state$assay_info
+    assay_info. <- assay_info()
     if (!is(assay_info., "facile_frame")) {
       # Creates a 0-row tibble with correct columns
       out <- collect(assay_feature_info(rfds, default_assay(rfds)), n = 1L)
